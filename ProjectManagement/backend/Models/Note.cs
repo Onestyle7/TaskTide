4b43953897850d963753e0c8a04c60badf225df9
Onestyle7 <klusbartek8@gmail.com>
2024-07-08 21:14:56 +0200

    update Note model validation
    - Removed unnecessary validation attribute from `User` property
    - Ensured `Title` is required with appropriate error message
    - Ensured `Priority` has a valid range between 1 and 5 with appropriate error message

--- ProjectManagement/backend/Models/Note.cs
+++ ProjectManagement/backend/Models/Note.cs
@@ -5,9 +5,10 @@ namespace ProjectManagement.backend.Models
     public class Note
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = " The Title field is required.")]
         public string Title { get; set; }
         public string Content { get; set; }
-        [Range(1,5)]
+        [Range(1,5, ErrorMessage = "The field Priority must be between 1 and 5.")]
         public int Priority { get; set; }
         public string UserId { get; set; }
         public ApplicationUser User { get; set; }
